6e6d776a3c2cb204da2d1743c0998712a4fc4fc2
hiroya-hinomori <hiroya.hinomori@kinto-technologies.com>
2022-09-13 14:30:42 +0900

    Initial commit

---
+++ TCAPlaygrounds/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TCAPlaygrounds
+//
+//  Created by 日野森 寛也（Hiroya Hinomori） on 2022/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCAPlaygrounds/TCAPlaygroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPlaygroundsApp.swift
+//  TCAPlaygrounds
+//
+//  Created by 日野森 寛也（Hiroya Hinomori） on 2022/09/13.
+//
+
+import SwiftUI
+
+@main
+struct TCAPlaygroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
